e48756f84b9fcd136d0bb7b5e29901d1954cee50
michelle <michelletyson29@gmail.com>
2019-01-17 11:34:35 -0800

    start pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+//create start button
+//create array with multiple choice questions and answer
+//event to capture the users answer choice for each question
+//only allow one answer per question 
+//timer countdown 
+//when time ends, compare the submitted answers to the true answer
+//unanswered questions are counted as wrong 
+//display the number of correct and wrong answers
+
+
+
